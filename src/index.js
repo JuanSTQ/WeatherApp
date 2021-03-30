7c9516380ad2f0745d786b927b0f39520142c47f
Juan Tello <juanstq20@gmail.com>
2021-03-30 18:50:00 -0500

    Get data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 
+import { getData } from './utils/getData';
+
+
 
 
 (async function App(){
- const input = document.querySelector('input#input__city')
+  const input = document.querySelector('input#input__city')
   const button = document.querySelector('button.button__submit')
   const form = document.querySelector('form.container__form')
+  const render = document.querySelector('.render')
 
-
-  const getText = (event)=>{
-    const id = getId(event.target.value)   
+  const getText = async (event)=>{
+    const cityName = input.value;
+    const view = await getData(cityName) ;
+    render.append(view)
   }
 
-
-  input.addEventListener('input',change)
   button.addEventListener('click', getText)
   form.addEventListener('submit', (event)=>{event.preventDefault()})
 })();
